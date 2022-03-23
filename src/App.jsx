687672751f2e056b8a4624f47efec9eeac61f6dd
SC3 <ceosmarck@gmail.com>
2022-03-23 10:03:12 -0600

    input

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+let varprueba = "Titulo";
+
+const handleChangePrueba = (event) => {
+  
+  varprueba = event.target.value;
+  //console.log(event.target.name, event.target.value);
+};
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
+      <label htmlFor="pruebaID">Input de pruba</label>
+      <input 
+
+      id="pruebaID" 
+      name="prueba" 
+      type="text" 
+
+      onChange={handleChangePrueba} 
+      value = {varprueba} />
+      
     </div>
+    
   );
 }
-
 export default App;
+  
