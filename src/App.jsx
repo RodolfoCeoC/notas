f7e5139291b0cde48c6f463d8a13a22feca05639
SC3 <ceosmarck@gmail.com>
2022-03-29 09:13:32 -0600

    tercer commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,75 @@
+import {useState} from 'react'
 
 function App() {
-let varprueba = "Titulo";
+
+const [pruebaState, setPruebaState] = useState("");
+const [tituloState, setTituloState] = useState("");
+const [fechaState, setFechaState] = useState("");
+const [notaState, setNotaState] = useState("");
+
+  //let varprueba = "Titulo";
 
 const handleChangePrueba = (event) => {
+  setPruebaState(event.target.value);
   
-  varprueba = event.target.value;
   //console.log(event.target.name, event.target.value);
 };
 
+const handleChangeTitulo = (event) => {
+  setTituloState(event.target.value);
+  
+  //console.log(event.target.name, event.target.value);
+};
+
+const handleChangeFecha = (event) => {
+  setFechaState(event.target.value);
+  
+  //console.log(event.target.name, event.target.value);
+};
+
+const handleChangeNota = (event) => {
+  setNotaState(event.target.value);
+  
+  //console.log(event.target.name, event.target.value);
+};
   return (
     <div className="App">
       <h1>Hello</h1>
-      <label htmlFor="pruebaID">Input de pruba</label>
-      <input 
 
+      <label htmlFor="pruebaID">Input de prueba</label>
+      <input 
       id="pruebaID" 
       name="prueba" 
       type="text" 
-
       onChange={handleChangePrueba} 
-      value = {varprueba} />
+      value = {pruebaState} />
+<br />
+<br />
+      <label htmlFor="titulo">Titulo</label>
+      <input 
+      id="titulo" 
+      name="titulo" 
+      type="text" 
+      onChange={handleChangeTitulo} 
+      value = {tituloState} />
+<br />
+<br />
+      <label htmlFor="fecha">Fecha</label>
+      <input 
+      id="fecha" 
+      name="fecha" 
+      type="text" 
+      onChange={handleChangeFecha} 
+      value = {fechaState} />
+<br />
+<br />
+      <label htmlFor="nota">Nota</label>
+      <input 
+      id="nota" 
+      name="nota" 
+      type="text" 
+      onChange={handleChangeNota} 
+      value = {notaState} />
       
     </div>
     
